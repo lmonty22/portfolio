e3311e229017350e8cfd1b888a8f89d33103054e
lmonty22 <lindsay.montgomery@olympian.org>
2020-08-04 10:20:54 -0400

    blog update, added read more button;

--- src/containers/BlogContainer.js
+++ src/containers/BlogContainer.js
@@ -1,20 +1,19 @@
 import React from 'react'
-import {Row, Container} from 'react-bootstrap'
+import {Row, Container, Button} from 'react-bootstrap'
 import Blog from '../components/Blog'
 import { Slide } from "react-awesome-reveal";
 
 const Blogs = [
+    {   title: 'JavaScript Basics & Theory',
+        link: 'https://medium.com/@lmonty22/javascript-basics-theory-28f2696a00d1',
+        date: 'August, 4, 202xs0',
+        image: 'https://miro.medium.com/max/1400/1*2LCM6SQcbNr1O1KnXV97uQ.png'
+    },
     {   title: 'Hooked on Hooks',
         link: 'https://medium.com/@lmonty22/hooked-on-hooks-9590b941596e',
         date: 'July, 27 2020',
         image: 'https://miro.medium.com/max/1400/1*1LPM2n_d7qAsJZIXbviOyQ.png'
-    },   
-    {
-    title: 'Shell Scripting made Simple',
-    link: 'https://medium.com/@lmonty22/shell-scripting-made-simple-efe6f6b69043',
-    date: 'July 20, 2020',
-    image: 'https://miro.medium.com/max/1400/1*N201ChnesHowJ32m1Vrolw.jpeg'
-    },  
+    },    
     {
     title: 'Big O Notation & Avoiding Nested Loops',
     link: 'https://medium.com/@lmonty22/big-o-notation-avoiding-nested-loops-2924f3002a08',
@@ -30,6 +29,9 @@ const BlogContainer = (props) => {
             <h1 >Blogs</h1>
             <Row className='blogs-row'>
                 {Blogs.map( b=> <Blog blog={b} />)}
+            </Row>
+            <Row className='blogs-row'>
+            <a target="_blank" rel="noopener noreferrer" href='https://medium.com/@lmonty22'><Button>Read more</Button></a>
             </Row>
         </Container>
         </Slide>
